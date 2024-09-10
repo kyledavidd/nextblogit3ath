60eb55fbd4866af98d8665a0eb9eca8d99ffe117
Jordan Micko Deloria <deloriamicko@gmail.com>
2024-09-10 12:42:14 +0800

    first commit

--- pages/index.js
+++ pages/index.js
@@ -9,33 +9,27 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-       <hr></hr>
+        <p>10 Things That Require Zero Talent
+        </p>
+
+        <ul>
+          <li>Being On Time</li>
+          <li>Making An Effort</li>
+          <li>Being High Energy</li>
+          <li>Having A Positive Attitude</li>
+          <li>Being Passionate</li>
+          <li>Using Good Body Language</li>
+          <li>Being Coachable</li>
+          <li>Doing a Little Extra</li>
+          <li>Being Prepared</li>
+          <li>Having A Strong Work Ethic</li>
+        </ul>
+
+
         <p>
-          (Hello! I'm <strong><em>Yhuan Kyle David</em></strong>, a 3rd year BSIT student at the University of the Assumption. I'm from San Matias, Santo Tomas, Pampanga. My hobbies are playing video games and reading anime manga. I enjoy listening to old music like The Eagles, Queen and many other artists. My favorite food is pizza. I love dogs, and I have one named Olatut.</p>
-
-          <hr></hr>
-
-          
-          <p>I took up IT because there’s a lot of opportunities in this industry and it's also a high paying job.
-          </p>
-
-          <hr></hr>
-
-          <p>I hope that I work in the IT industry and will try different fields in IT and see what's best for me.</p>
-
-          <hr></hr>
-
-          <p>I want to learn more about how to create a website and learn the basics of how to make one and eventually get better at it.</p> 
-
-          <hr></hr>
-
-          <p>The topics I want to learn more is about web development and all the applications that are going to be used.</p>
-          
-                    
-           
-            (This is a sample website - you’ll be building a site like this on{' '}
+          (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        
+        </p>
       </section>
     </Layout>
   );
